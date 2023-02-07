57cf337bd7e7bf04c1a56efd840c41929eec1a72
NMBxNIGHT <thiranat_kakanmee@cmu.ac.th>
2023-02-07 16:39:26 +0700

    3_4 completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,34 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d){
+	int x=rand()%3,temp1,temp2;
+	switch (x	)
+	{
+	case 0:
+		temp1 =a;
+		a=b;
+		b=c;
+		c=d;
+		d=temp1;
+		break;
+	case 1:
+		temp1 =b;temp2=a;
+		a=c;
+		b=d;
+		c=temp2;
+		d=temp1;
+		break;
+	default:
+		temp1 =a,temp2=b;
+		a=d;
+		d=c;
+		b=temp1;
+		c=temp2;
+
+	}
+	
+
+
+}
